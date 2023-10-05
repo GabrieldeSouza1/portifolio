6cbdcd8c1e3c29d134b5a390f552ea281b457b8d
GabrieldeSouza1 <gabrieldesouza125@gmail.com>
2023-10-05 19:54:50 -0300

    FIX: Caminho

--- portifolio/portifolio/src/components/projectGroup/projectGroup.jsx
+++ portifolio/portifolio/src/components/projectGroup/projectGroup.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import ProjectCard from '../ProjectCard/ProjectCard'
+import ProjectCard from '../projectCard/ProjectCard'
 
 import Styles from './projectGroup.module.css'
 
